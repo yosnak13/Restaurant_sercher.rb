ef76a25fd10012f1e60c7c53844a9502ecc5ba33
yosnak13 <yosnak20@gmail.com>
2020-12-20 15:02:25 +0900

    create array.rb

---
+++ array.rb
@@ -0,0 +1,18 @@
+# customer1 = "侍 太郎"
+# customer2 = "侍 次郎"
+# customer3 = "侍 三郎"
+
+# puts customer1
+# # => 侍 太郎
+
+# puts customer2
+# # => 侍 次郎
+
+# puts customer3
+# # => 侍 三郎
+
+customers = ["侍 太郎", "侍 次郎", "侍 三郎"]
+
+for customer in customers do
+    puts customer
+end
