a62639d87df352d1447b7e439eebd55ca08f24ec
yosnak13 <yosnak20@gmail.com>
2020-12-21 13:35:05 +0900

    elsif sentence

--- if.rb
+++ if.rb
@@ -10,8 +10,17 @@ num = 10
 # end
 
 # else文
-if num > 10 then
-  puts "numは10より大きい"
-else
-  puts "numは10以下"
-end
+# if num > 10 then
+#   puts "numは10より大きい"
+# else
+#   puts "numは10以下"
+# end
+
+# elsif文
+# if num > 10 then
+#   puts "numは10より大きい"
+# elsif num < 10 and num < 5
+#   puts "numは10より小さく、かつ5より小さい"
+# else
+#   puts "numは10です"
+# end
