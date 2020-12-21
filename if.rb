537a4459a363b4ea0d663d3a81439f8026baa0ba
yosnak13 <yosnak20@gmail.com>
2020-12-21 13:32:59 +0900

    else sentence

--- if.rb
+++ if.rb
@@ -1,5 +1,17 @@
 num = 10
 
-if num >= 10 then
-  puts 'Hello World'
+# if num >= 10 then
+#   puts 'Hello World'
+# end
+
+# 複数条件
+# if num > 5 and num < 12 then
+#     puts "Hello World!"
+# end
+
+# else文
+if num > 10 then
+  puts "numは10より大きい"
+else
+  puts "numは10以下"
 end
