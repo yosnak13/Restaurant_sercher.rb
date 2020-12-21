3a24961930cceec94921421bb57f82bd2c173a32
yosnak13 <yosnak20@gmail.com>
2020-12-21 13:48:30 +0900

    if文の演習2

--- if.rb
+++ if.rb
@@ -27,10 +27,29 @@ num = 10
 
 
 # 演習1　引数numの値が42の場合のみ、Answer to the Ultimate Question of Life, the Universe, and Everythingと出力するメソッドcheckを作成。
-def check(num)
-  if num == 42
-    puts 'Answer to the Ultimate Question of Life, the Universe, and Everything'
+# def check(num)
+#   if num == 42
+#     puts 'Answer to the Ultimate Question of Life, the Universe, and Everything'
+#   end
+# end
+
+# check(42)
+
+
+# 演習2　空の配列enev_numとodd_numを作成。even_numは奇数の配列で、odd_numは偶数の配列。次に、引数の値が偶数の時と奇数の時で条件分岐させ、その値を作成したそれぞれのメソッドsort_numberを作成。
+
+even_num = []
+odd_num = []
+
+def sort_number(num, even_num, odd_num)
+  if num % 2 == 0
+    even_num.push(num)
+  else
+    odd_num.push(num)
   end
 end
 
-check(42)
+sort_number(21, even_num, odd_num)
+sort_number(42, even_num, odd_num)
+
+p odd_num
