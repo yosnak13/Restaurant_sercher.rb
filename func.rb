416bf453bf73e66de3db0ac33fb473ef16d1d141
yosnak13 <yosnak20@gmail.com>
2020-12-21 09:34:09 +0900

    演習前

---
+++ func.rb
@@ -0,0 +1,37 @@
+# 引数のあるメソッド
+# def hello(str)
+#   puts str
+# end
+
+# hello('Hello World!')
+
+# 引数なしのメソッド
+# def hello
+#   puts 42
+# end
+# hello
+
+# 引数が複数あるメソッド
+# def hello(a, b)
+#   puts a + b
+# end
+
+# hello(21, 29)
+
+# メソッドに初期値あり
+# def hello(a = 21)
+#   puts a
+# end
+# # メソッド実行時に引数が渡されなかった場合に、デフォルトで初期値が適用される
+# hello("Hello")
+
+# 戻り値
+# def add(a, b)
+#   return a + b
+# end
+
+# num = add(2, 3)
+# puts num
+
+# 演習
+
