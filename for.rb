00d0b0da5e5d719d0ee5e8fbe137e29ea6e4501d
yosnak13 <yosnak20@gmail.com>
2020-12-22 14:27:51 +0900

    配列への挿入をやってみた

--- for.rb
+++ for.rb
@@ -1,4 +1,15 @@
+# 基本のfor文
+# for num in 1..10 do
+#   puts 'Hello World!'
+# end
 
-for num in 1..10 do
-  puts 'Hello World!'
-end
+animals = ['dog', 'cat', 'bird']
+
+# for animal in animals do
+#   puts animal
+# end
+
+# animals.insert(2, 'lion')
+
+# puts animals
+
