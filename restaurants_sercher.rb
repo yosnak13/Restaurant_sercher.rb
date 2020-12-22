0291b605ac815114796461f3d5691005288981e9
yosnak13 <yosnak20@gmail.com>
2020-12-22 14:19:15 +0900

    errorでの条件分岐

--- restaurants_sercher.rb
+++ restaurants_sercher.rb
@@ -1,7 +1,12 @@
 def write_data_to_csv()
   restaurants = []
-  response = 'hogehoge'
+  response = {hogehoge:'hogehoge'}
+  if response.has_key?('error')then
+    puts "エラーが発生しました"
+  end
+
   return print restaurants
+end
 
 write_data_to_csv
 
