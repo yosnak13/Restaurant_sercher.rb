337ba4fe77b4731fb9791a2a11240a579d0c4972
yosnak13 <yosnak20@gmail.com>
2020-12-25 09:32:13 +0900

    hogeクラスを作成し、クラス内でhelloメソッドを定義

---
+++ class2.rb
@@ -0,0 +1,21 @@
+# class Human
+#   @@class_name = 'Human'
+
+#   def initialize
+#     @name = "大泉"
+#   end
+
+#   def show()
+#     puts @name
+#   end
+# end
+
+
+class Hoge
+  def hello
+    puts "Hello Ruby!"
+  end
+end
+
+hoge = Hoge.new
+hoge.hello
